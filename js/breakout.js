6329e7510b600972e12dd1bb402743d662fe3173
Timothy Reaport <142744826+timothyreaport@users.noreply.github.com>
2024-05-21 00:58:25 +0000

    Mon, May 20, 2024, 5:58 PM -07:00

---
+++ js/breakout.js
@@ -0,0 +1,19 @@
+rulesBtn = document.getElementById('rules-btn')
+rules = document.getElementById('rules')
+closeBtn = document.getElementById('close-btn')
+canvas = document.getElementById('canvas')
+ctx = canvas.getContext('2d')
+ball = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    size: 10,
+    speed: 4,
+    dx: 4;
+    dy; -4;
+}
+rulesBtn.addEventListener('click', () => {
+    rules.classList.add('show')
+})
+closeBtn.addEventListener('click', () => {
+    rules.classList.remove('show')
+})
